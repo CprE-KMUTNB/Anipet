7ac7030fc69fde5fe365904b6d1c9f6aaad6a3ce
Sang rongyong <s6401012620242@email.kmutnb.ac.th>
2022-10-04 00:13:26 +0700

    register

--- TEST/anipet/model/model.js
+++ TEST/anipet/model/model.js
@@ -1,28 +1,35 @@
-    const mongoose = require('mongoose')
-    const mongo = require('mongodb')
-    const dbUrl = 'mongodb://localhost:27017/BlogDB'
+const mongoose = require('mongoose')
+const mongo = require('mongodb')
+const dbUrl = 'mongodb://localhost:27017/BlogDB'
     
-    mongoose.connect(dbUrl,{
-        useNewUrlParser:true
-    })
-    const db = mongoose.connect
-    const Schema = mongoose.Schema
+mongoose.connect(dbUrl,{
+    useNewUrlParser:true
+})
+const db = mongoose.connect
+const Schema = mongoose.Schema
 
-    const blogSchema =new Schema({
-        id:{
+const blogSchema =new Schema({
+    id:{
             type:Schema.ObjectId
-        },
-        username:{
-            type:String,
-            require:true
-        },
-        password:{
-            type:String,
-            require:true
-        },
-        gender:{
-            type:String,
-            require:true
-        },
-    })
-    const Blogs = module.exports=mongoose.model("userId",blogSchema)
+    },
+    name:{
+        type:String,
+        require:true
+    },
+    username:{
+        type:String,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true
+    },
+    gender:{
+        type:String,
+        require:true
+    },
+})
+const Blogs = module.exports=mongoose.model("userId",blogSchema)
+module.exports.createBlog= function(newBlock,callback){
+    newBlock.save(callback)
+}
--- TEST/anipet/routes/blogs.js
+++ TEST/anipet/routes/blogs.js
@@ -9,5 +9,16 @@ router.get('/', function(req, res, next) {
 router.get('/add', function(req, res, next) {
   res.render("blog/addId",{data:"สมัครสมาชิก"})
 });
-  
+router.post('/add', function(req, res, next) {
+  data =new Blogs({
+    name:req.body.name,
+    username:req.body.username,
+    password:req.body.password,
+    gender:req.body.gender
+  })
+  Blogs.createBlog(data,function(err){
+    if(err) console.log(err);
+    res.redirect("/blogs")
+  });
+});
 module.exports = router;  
