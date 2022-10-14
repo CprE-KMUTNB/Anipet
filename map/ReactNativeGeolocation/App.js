aa39dd3f5b50c68abdb890dcf3195358e84d7bb3
Sang rongyong <s6401012620242@email.kmutnb.ac.th>
2022-10-14 16:35:37 +0700

    front endmap

--- map/ReactNativeGeolocation/App.js
+++ map/ReactNativeGeolocation/App.js
@@ -1,5 +1,4 @@
-/* eslint-disable react/jsx-no-undef */
-/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable no-unused-vars */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -21,11 +20,12 @@ import {
 } from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
-// Function to get permission for location
-const {width, height} = Dimensions.get('window');
+import {googleAPIKey, placeType} from '../../Helpers/GooglePlacesAPI';
 
+const {width, height} = Dimensions.get('window');
 const SCREEN_HEIGHT = height;
 const SCREEN_WIDTH = width;
+
 const requestLocationPermission = async () => {
   try {
     const granted = await PermissionsAndroid.request(
@@ -49,18 +49,77 @@ const requestLocationPermission = async () => {
 };
 
 const App = () => {
-  const mapRef = useRef(null);
-  const placesRef = useRef();
-  const getAddress = () => {
-    console.log(placesRef.current?.getAddressText());
-  };
+  const [Search, Location] = useState({
+    latitude: 15,
+    longitude: 100,
+    latitudeDelta: 0.001,
+    longitudeDelta: 0.001,
+  });
   const [position, setPosition] = useState({
     latitude: 15,
     longitude: 100,
     latitudeDelta: 0.001,
     longitudeDelta: 0.001,
   });
+  const mapRef = useRef(null);
+  const placesRef = useRef();
   useEffect(() => {
+    const latitude = position.latitude; // you can update it with user's latitude & Longitude
+    const longitude = position.longitude;
+    let radMetter = 5 * 1000; // Search withing 5 KM radius
+    const url =
+      'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' +
+      latitude +
+      ',' +
+      longitude +
+      '&radius=' +
+      radMetter +
+      '&type=hospital' +
+      '&key=' +
+      'AIzaSyDziN8yZ8H1h7yOLyxeRQyoySDMlWZXIJc';
+
+    fetch(url)
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        var places = []; // This Array WIll contain locations received from google
+        for (let googlePlace of res.results) {
+          var place = {};
+          var lat = googlePlace.geometry.location.lat;
+          var lng = googlePlace.geometry.location.lng;
+          var coordinate = {
+            latitude: lat,
+            longitude: lng,
+          };
+
+          var gallery = [];
+
+          if (googlePlace.photos) {
+            for (let photo of googlePlace.photos) {
+              var photoUrl = url.GooglePicBaseUrl + photo.photo_reference;
+              gallery.push(photoUrl);
+            }
+          }
+
+          place.placeTypes = googlePlace.types;
+          place.coordinate = coordinate;
+          place.placeId = googlePlace.place_id;
+          place.placeName = googlePlace.name;
+          place.gallery = gallery;
+
+          places.push(place);
+        }
+        console.log(
+          `The places around thailand: ${places.map(
+            nearbyPlaces => nearbyPlaces.placeName,
+          )}`,
+          // Do your work here with places Array
+        );
+      })
+      .catch(error => {
+        console.log(error);
+      });
     const result = requestLocationPermission();
     result.then(res => {
       if (res) {
@@ -72,10 +131,16 @@ const App = () => {
             latitudeDelta: 0.0421,
             longitudeDelta: 0.0421,
           });
+          Location({
+            latitude: crd.latitude,
+            longitude: crd.longitude,
+            latitudeDelta: 0.0421,
+            longitudeDelta: 0.0421,
+          });
         });
       }
     });
-  }, []);
+  }, [position.latitude, position.longitude]);
   return (
     <View style={styles.container}>
       <MapView
@@ -92,7 +157,7 @@ const App = () => {
         <MarkerAnimated
           title="Yor are here"
           //  description='This is a description'
-          coordinate={position}
+          coordinate={Search}
         />
       </MapView>
       <View style={styles.searchContainer}>
@@ -102,12 +167,12 @@ const App = () => {
           styles={{textinput: styles.input}}
           placeholder="Search"
           query={{
-            key: 'process.env.API',
+            key: 'AIzaSyDziN8yZ8H1h7yOLyxeRQyoySDMlWZXIJc',
             components: 'country:th',
             language: 'en', // language of the results
           }}
           onPress={(data, details = null) => {
-            setPosition({
+            Location({
               latitude: details?.geometry?.location?.lat,
               longitude: details?.geometry?.location?.lng,
               latitudeDelta: 0.0421,
