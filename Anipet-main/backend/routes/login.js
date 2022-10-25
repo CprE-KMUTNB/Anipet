682f6e43606254b9f949899d9f8eeb2e84f3f1d3
Theethawach W <99662035+kimzaooza@users.noreply.github.com>
2022-10-25 23:46:34 +0700

    Revert "fix setting"
    This reverts commit 72041fc74e31a49790468887c4a966ea9d83882a.

--- Anipet-main/backend/routes/login.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const {login,logout} = require("../controller/login")
-
-router.post('/login',login);
-
-router.delete('/logout',logout);
-
-module.exports = router
